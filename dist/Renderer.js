233858f392e498d56d2bbe44a5d6431ee65f7ea5
ahmad dregat <ahmad.dregat.97@hotmail.com>
2021-06-06 22:06:02 +0300

    pla ppla

---
+++ dist/Renderer.js
@@ -0,0 +1,42 @@
+class Render {
+    constructor() {
+
+    }
+    renderData = function(items) {
+        const source = $("#store-template").html()
+        const template = Handlebars.compile(source)
+        let itemSheet = template({ items })
+        $(".menu").empty()
+        $(".menu").append(itemSheet)
+    }
+
+
+
+    renderLoginPage = function() {
+        const source = $("#Login-template").html()
+        const template = Handlebars.compile(source)
+        let itemSheet = template()
+        $(".menu").empty()
+        $(".menu").append(itemSheet)
+    }
+
+    renderSignUpPage = function() {
+        const source = $("#SignUp-template").html()
+
+
+
+        const template = Handlebars.compile(source)
+        let itemSheet = template()
+        $(".menu").empty()
+        $(".menu").append(itemSheet)
+    }
+    renderDataCart = function(items) {
+        const source = $("#cart-template").html()
+        const template = Handlebars.compile(source)
+        let itemSheet = template({ items })
+        $(".menu").empty()
+        $(".menu").append(itemSheet)
+    }
+
+
+}
