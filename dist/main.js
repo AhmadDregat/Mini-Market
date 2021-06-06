36b0b9214065bc73a54933897278231f5cfd28dc
bahaabder <bahaa.bder97@gmail.com>
2021-06-06 17:40:27 +0300

    adding menu

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,24 @@ $.ajax({
     type: 'get',
     async: false,
     success: function(data) {
-        // console.log(data)
         render.renderData(data)
     }
 });
+
+const myFunction = function() {
+    $("#navbar").slideToggle(1000);
+}
+
+
+const myHome = function() {
+    $.ajax({
+        url: '/items',
+        type: 'get',
+        async: false,
+        success: function(data) {
+
+            render.renderData(data)
+        }
+    });
+
+}
