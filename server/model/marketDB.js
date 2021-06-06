086bed62b474114bf9e22c0ca0f965588833fb73
ahmad dregat <ahmad.dregat.97@hotmail.com>
2021-06-06 10:10:29 +0300

    setup the Db

--- server/model/marketDB.js
+++ server/model/marketDB.js
@@ -1,1 +1,14 @@
- 
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+
+const marketSchema = new Schema({
+    name: String,
+    price: Number,
+    quantity: String,
+    itemimg: String
+})
+
+const market = mongoose.model("market", marketSchema)
+module.exports = market
